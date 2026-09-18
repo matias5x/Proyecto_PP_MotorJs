569a07fdc600033603c4dea6e94c63054b70fd74
matias5x <matias140907r@gmail.com>
2026-09-18 03:41:25 -0300

    Enhance motor.js with input display functionality
    Updated the motor loop to display mouse and keyboard inputs on the canvas.

--- Proyecto Practicas Prof/Motor/js/motor.js
+++ Proyecto Practicas Prof/Motor/js/motor.js
@@ -21,20 +21,36 @@ let bucle = {
     play: function() {
         bucle.fps++;
         borrarCanvas();
+        //ctx.beginPath();
+
+        //ctx.font = "8px Verdana";
+        //ctx.fillStyle = "yellow";
+        //ctx.fillText(Timer.now, 10, 10);
+
+        //ctx.stroke();
+//prebas de inputs:
         ctx.beginPath();
-        ctx.fillStyle = "white";
-        ctx.font = "8px Arial";
-        ctx.fillText(Timer.now, 10, 10);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.fillStyle = "white";
-        ctx.font = "8px Arial";
-        ctx.fillText(myReloj.view, 10, 40);
-
-
-        ctx.stroke();
-        
+        ctx.font = "7px Verdana";
+        ctx.fillStyle = "yellow";
+        ctx.fillText("Mouse X: " + Mouse.x, 10, 20);
+        ctx.fillText("Mouse Y: " + Mouse.y, 10, 30);
+        ctx.fillText("Mouse ScreenX: " + Mouse.screenX, 10, 40);
+        ctx.fillText("Mouse ScreenY: " + Mouse.screenY, 10, 50);
+        ctx.fillText("Mouse MovementX: " + Mouse.movementX, 10, 60);
+        ctx.fillText("Mouse MovementY: " + Mouse.movementY, 10, 70);
+        ctx.fillText("Mouse Button: " + Mouse.button, 10, 80);
+        ctx.fillText("Mouse AltKey: " + Mouse.altKey, 10, 90);
+        ctx.fillText("Mouse CtrlKey: " + Mouse.ctrlKey, 10, 100);
+        ctx.fillText("Mouse ShiftKey: " + Mouse.shiftKey, 10, 110);
+        ctx.fillText("Mouse OnDown: " + Mouse.onDown, 10, 120);
+        ctx.fillText("Mouse OnUp: " + Mouse.onUp, 10, 130);
+        ctx.fillText("keyboard.key" + Keyboard.key, 100, 20)
+        ctx.fillText("keyboard.keyCode" + Keyboard.keyCode, 100, 30)
+        ctx.fillText("array de keys" + Keyboard.arrayKeys, 100, 40)
+        ctx.fillText("flecha der" + Keyboard.keys["arrowLeft"], 100, 50)
+        ctx.fillText("flecha izq" + Keyboard.keys["arrowRight"], 100, 60)
+        ctx.fillText("flecha arriba" + Keyboard.keys["arrowUp"], 100, 70)
+        ctx.fillText("flecha abajo" + Keyboard.keys["arrowDown"], 100, 80)
     },
 }
 let canvas = document.getElementById("canvas");
@@ -52,6 +68,8 @@ function ajustarVentana() {
     canvas.style.width = ancho + "px";
     canvas.style.height = alto + "px";
 };
+Concurrent.Thread.create(InitTimer);
+Keyboard.load();
 
 function borrarCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -64,7 +82,4 @@ window.addEventListener("load", function(e) {
 window.addEventListener("resize", function(e) {
     ajustarVentana();
 });
-Concurrent.Thread.create(InitTimer);
 
-var myReloj = new Cronometro("crono1", tipoCrono.Forward);
-myReloj.run();
